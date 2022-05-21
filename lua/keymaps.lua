43e513fc6b739cc4573dd40d47b79a1ebc9e5931
David Ballen <juandbm2001@gmail.com>
2022-05-21 18:46:13 -0500

    first commit

---
+++ lua/keymaps.lua
@@ -0,0 +1,48 @@
+local mapper = function (mode, key, result)
+	vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
+end
+
+vim.g.mapleader = ' '
+
+mapper('i', '<Leader><Leader>', '<cmd>w <CR><Esc>')
+mapper('n', '<C-S>', ':w! <CR>')
+
+mapper('n', '<C-w>', ':w <CR>')
+mapper('n', '<C-q>', ':q <CR>')
+mapper('n', '<Leader><Right>', '20 <C-w>>')
+mapper('n', '<Leader><Left>', '20 <C-w><')
+
+-- Text editor
+mapper('n', '<C-a>', 'gg<S-v>G')
+mapper('n', '<leader>d', ':t. <CR>')
+
+-- Telescope
+mapper('n', '<Leader>ff', ':Telescope find_files <CR>')
+mapper('n', '<Leader>fg', ':Telescope live_grep <CR>')
+mapper('n', '<C-]>', ':Telescope buffers <CR>')
+mapper('n', '<Leader>fh',':Telescope help_tags <CR>')
+
+mapper('n','<Leader>fd', ':Telescope file_browser <CR>')
+mapper("n", "<C-f>", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>") -- search lines in current buffer
+mapper("n", "<Leader>fr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>") -- search references to symbol under cursor
+mapper("n", "<Leader>co", "<cmd>lua require('telescope.builtin').colorscheme()<CR>") -- colorschemes
+mapper("n", "<Leader>gc", "<cmd>lua require('telescope.builtin').git_branches()<CR>") -- checkout different branches
+mapper("n", "<Leader>re", "<cmd>lua require('telescope.builtin').git_commits()<CR>") -- checkout commits; <CR> to checkout, <C-r>[m, s, h] to reset [mixed, soft, hard]
+mapper("n", "<Leader>qf", "<cmd>lua require('telescope.builtin').quickfix()<CR>") -- jump to items in quickfix list
+mapper("n", "H", "<cmd>lua require('telescope.builtin').lsp_code_actions(require('telescope.themes').get_cursor())<CR>") -- code actions
+
+-- nvim-tree
+
+mapper('n', '<C-n>', ':NvimTreeToggle <CR>')
+mapper('n', '<Leader>r', ':NvimTreeRefresh <CR>')
+mapper('n', '<Leader>n', ':NvimTreeFindFile <CR>')
+
+mapper('n', '<C-z>', ':TSToggle highlight <CR>')
+
+-- Bracey config (Live server) --
+
+mapper('n', '<Leader>bs', ': Bracey <CR>')
+mapper('n', '<Leader>bp', ': BraceyStop <CR>')
+mapper('n', '<Leader>br', ': BraceyReload <CR>')
+
+mapper('n', '<Leader>F', ':CocCommand prettier.formatFile <CR>')
---
+++ lua/plugins/init.lua
@@ -0,0 +1,145 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+return packer.startup(function(use)
+  -- My plugins here
+  use "wbthomason/packer.nvim" -- Have packer manage itself 
+
+use {
+   'windwp/nvim-autopairs',
+    config = require "plugins.configs.autopairs"
+}  
+  use {
+      "nvim-telescope/telescope.nvim",
+      requires = {
+        {'nvim-lua/popup.nvim'},                                 
+        {'nvim-lua/plenary.nvim'},                               
+        {'nvim-telescope/telescope-fzf-native.nvim', run="make"},
+        {'nvim-telescope/telescope-symbols.nvim'},
+        { "nvim-telescope/telescope-file-browser.nvim" }, 
+      },
+      config = require "plugins.configs.telescope",
+    }
+
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      requires = {
+        "windwp/nvim-ts-autotag",
+        "p00f/nvim-ts-rainbow",
+      },
+      run = ':TSUpdate',
+      config = require "plugins.configs.treesitter"
+  }
+
+  use {
+    "williamboman/nvim-lsp-installer",
+      requires = {
+          "neovim/nvim-lspconfig",
+          "jose-elias-alvarez/typescript.nvim",
+      },
+      config = require "plugins.configs.lsp"
+    }
+
+    use { 'ibhagwan/fzf-lua',
+    -- optional for icon support
+    requires = { 'kyazdani42/nvim-web-devicons' }
+  }  
+
+  -- UI
+
+  use { "rmehri01/onenord.nvim" }
+  use { "goolord/alpha-nvim", config = require "plugins.configs.alpha" }
+  use { "kyazdani42/nvim-web-devicons", config = require "plugins.configs.icons" }
+  use {
+      "nvim-lualine/lualine.nvim",
+       config = require "plugins.configs.lualine",
+      requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  }
+
+  use {
+      "hrsh7th/nvim-cmp",
+      requires = {
+          "hrsh7th/cmp-nvim-lsp",
+          "hrsh7th/cmp-nvim-lua",
+          "hrsh7th/cmp-buffer",
+          "hrsh7th/cmp-path",
+          "hrsh7th/cmp-cmdline",
+          "hrsh7th/cmp-nvim-lsp-document-symbol",
+          "hrsh7th/cmp-vsnip",
+          "hrsh7th/vim-vsnip",
+          "hrsh7th/cmp-nvim-lsp-signature-help",
+      },
+      config = require "plugins.configs.cmp",
+  }
+
+  use {
+    "L3MON4D3/LuaSnip",
+    config = require "plugins.configs.luasnip"
+  }
+
+  use {"saadparwaiz1/cmp_luasnip"}
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = require "plugins.configs.nvim-tree",
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+    require('gitsigns').setup()
+  end
+   -- tag = 'release' -- To use the latest release 
+  }
+
+  use 'tpope/vim-fugitive'
+  use 'turbio/bracey.vim'
+  use 'sheerun/vim-polyglot'
+  use 'alvan/vim-closetag'
+  use 'tpope/vim-commentary'
+
+ if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
